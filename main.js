5b98b40d7ccf103d880bf3380555db0fca2d1b5f
thegod124 <81662245+thegod124@users.noreply.github.com>
2021-09-12 11:57:39 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image(); 
+	img_imgTag.onload = uploadimg; 
+	img_imgTag.src = img_image;   
+}
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keudown(e){
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	if(keyPressed >=97 && keyPressed<=122|| (keyPressed >=65 && keyPressed<=90)){
+		aplhabetkey();
+		document.getElementById("d1").innerHTML="You have pressed an alphabet key.";
+		console.log("alphabet key");
+	}
+}
+
+function aplhabetkey()
+{
+	img_image="Alpkey.png";
+	add(); 
+}
+function numberkey()
+{
+	img_image="numkey.png";
+	add(); 
+}
+function arrowkey()
+{
+	img_image="Arrkey.png";
+	add(); 
+}
+function specialkey()
+{
+	img_image="spkey.png";
+	add(); 	
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
+	
